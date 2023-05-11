6534757f71c8058b4beb76d0cbd8fa536fc71d61
Henrique <henrique.heid@gmail.com>
2023-05-11 17:07:30 -0300

    Arvv completa

--- AT7 - Arvore N-aria/arvv.c
+++ AT7 - Arvore N-aria/arvv.c
@@ -17,20 +17,29 @@ void arvv_insere(Arvv*a, Arvv*sa)
     a->prim = sa;
 }
 
-// Arvv* arvv_libera(Arvv* a)
-// {
+void arvv_libera(Arvv* a)
+{
+    if (a != NULL)
+    {
+        arvv_libera(a->prim);
+        arvv_libera(a->prox);
+    }
+    free(a);
+}
 
-// }
+
 int arvv_pertence(Arvv* a, int n)
 {
-    if (a->info == n)
-    {
-        return 1;
-    }
+
+    if (a->info == n) return 1;
+    //Cria um ponteiro temporario para o primeiro filho
     Arvv *temp = a->prim;
+    //Enquanto existir filho
     while (temp != NULL)
     {
+        //Se pertence retorna 1, recursivo ate encontrar uma folha
         if (arvv_pertence(temp,n)) return 1;
+        //Testa os proximos filhos
         temp = temp->prox;
     }
     return 0;
@@ -38,21 +47,96 @@ int arvv_pertence(Arvv* a, int n)
 
 void arvv_imprime(Arvv* a)
 {
-    if (a == NULL)
-    {
-        return;
-    }
+    if (a == NULL) return;
+    //Imprime a informacao de a
     printf("<%d", a->info );
+    //Recursao para o primeiro filho
     arvv_imprime(a->prim);
-    printf(">");
+
+    printf("> ");
     arvv_imprime(a->prox);
 }
 
 int arvv_altura(Arvv* a)
 {
-    
+    int altura_max = -1;
+    Arvv *temp = a->prim;
+    while(temp != NULL)
+    {
+        int altura_sub = arvv_altura(temp);
+        if (altura_sub > altura_max)
+        {
+            altura_max = altura_sub;
+        }
+        temp = temp->prox;
+    }
+    return altura_max + 1;
 }
-// int arvv_pares(Arvv* a);
-// int arvv_folhas(Arvv* a);
-// int arvv_ivual(Arvv* a1, Arvv* a2);
-// Arvv* arvv_copia(Arvv* a);
+
+int arvv_pares(Arvv* a)
+{
+    int pares = 0;
+    if (a->info % 2 ==0) pares++;
+    Arvv *temp = a->prim;
+    while(temp != NULL)
+    {
+        pares += arvv_pares(temp);
+        temp = temp->prox;
+    }
+    return pares;
+}
+
+int arvv_folhas(Arvv* a)
+{
+    int folhas = 0;
+    if (a->prim == NULL) folhas++;
+    Arvv *temp = a->prim;
+    while (temp != NULL)
+    {
+        folhas += arvv_folhas(temp);
+        temp = temp->prox;
+    }
+    return folhas;
+}
+
+int arvv_igual(Arvv* a1, Arvv* a2)
+{
+    if (a1 == NULL && a2 == NULL) return 1;
+
+    if ((a1 !=NULL && a2 != NULL) && (a1->info == a2->info))
+    {
+        if (arvv_igual(a1->prim,a2->prim)) return arvv_igual(a1->prox,a2->prox);
+    }
+
+    return 0;
+}
+
+Arvv* arvv_copia(Arvv* a)
+{
+    if (a == NULL) return NULL;
+
+    Arvv* novo = arvv_cria(a->info);
+    Arvv* temp = a->prim;
+    Arvv* ultimo_filho = NULL;
+
+    while (temp != NULL) 
+    {
+        Arvv* copia = arvv_copia(temp);
+        if (copia != NULL) 
+        {
+            if (ultimo_filho == NULL) 
+            {
+                novo->prim = copia;
+            } 
+            else 
+            {
+                ultimo_filho->prox = copia;
+            }
+            
+            ultimo_filho = copia;
+        }
+        temp = temp->prox;
+    }
+
+    return novo;
+}
