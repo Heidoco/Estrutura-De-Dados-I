f054802d022c46558b1a67c0793fcd66c5a57674
Henrique <henrique.heid@gmail.com>
2023-06-19 14:57:44 -0300

    Tabela Hash completo

--- AT10 - Tabela Hash/main.c
+++ AT10 - Tabela Hash/main.c
@@ -4,7 +4,15 @@ int main()
 {
     Hash tab[N];
     hsh_set(tab,10,"aaa","fjajdf@gmail.com", "A");
+    hsh_set(tab,2,"bbb","bbbb@gmail.com", "B");
+    hsh_set(tab,1,"ccc","ccccc@gmail.com", "C");
+    hsh_set(tab,22,"ddd","ddddd@gmail.com", "D");
+
     hsh_get(tab,10);
-    
+    printf("Buscar a matricula 10: %d\n",hsh_get(tab,10)->matricula);
+    hsh_remove(tab, 10);
+    printf("Matricula 10 removida: \n");
+    hsh_get(tab, 10);
+    hsh_imprime(tab);
     return 0;
 }
--- AT10 - Tabela Hash/tabela_hash.c
+++ AT10 - Tabela Hash/tabela_hash.c
@@ -5,12 +5,7 @@ static int hash(int k)
     return k%N;
 }
 
-Aluno* hsh_get(Hash tab, int mat)
-{
-    hash(mat);
-}
-
-Aluno* hsh_set(Hash tab, int mat,const char* n, char *e, char *t)
+Aluno* hsh_set(Hash tab, int mat, const char* n, char *e, char *t)
 {
     int h = hash(mat);
     Aluno *temp = tab[h];
@@ -32,7 +27,6 @@ Aluno* hsh_set(Hash tab, int mat,const char* n, char *e, char *t)
     temp->email = e;
     temp->nome  = n;
     temp->turma = t;
-    
 }
 
 Aluno* hsh_get(Hash tab, int mat)
@@ -43,9 +37,54 @@ Aluno* hsh_get(Hash tab, int mat)
     {
         if (temp->matricula == mat)
         {
-            printf("%d", temp->matricula);
             return temp;
         }
         temp = temp->prox;
+    }
+    return NULL;
+}
+
+void hsh_remove(Hash tab, int mat)
+{
+    int h = hash(mat);
+    Aluno *temp = tab[h];
+    Aluno* anterior = NULL;
+    while (temp != NULL)
+    {
+        if (temp->matricula == mat)
+        {
+            if (anterior == NULL)
+            {
+                tab[h] = temp->prox;
+            }
+            else
+            {
+                anterior->prox = temp->prox;
+            }
+            free(temp);
+            return;
+        }
+        anterior = temp;
+        temp = temp->prox;
+    }
+}
+
+void hsh_imprime(Hash tab)
+{
+    Aluno *temp;
+    for (int i = 0; i < N; i++)
+    {
+        if (tab[i] != NULL)
+        {
+            temp = tab[i];
+            while (temp != NULL)
+            {
+                printf("Matricula: %d\n", temp->matricula);
+                printf("Nome: %s\n", temp->nome);
+                printf("Turma: %s\n", temp->turma);
+                printf("Email: %s\n------- \n", temp->email);
+                temp = temp->prox;
+            }
+        }
     }
 }
